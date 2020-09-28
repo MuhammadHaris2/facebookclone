5667fef5036e52c61ad3c1e278b53632a33c8366
MuhammadHaris2 <mhari8339@gmail.com>
2020-09-28 09:39:31 +0500

    Updates

---
+++ precache-manifest.dc7d81caa87a7bf757f9902bc421c30c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72c5edcd0f76930f106708e39061d4dd",
+    "url": "/facebookclone/index.html"
+  },
+  {
+    "revision": "0d05bf40d0534acd2ed9",
+    "url": "/facebookclone/static/css/main.ecf52794.chunk.css"
+  },
+  {
+    "revision": "65781df3804465f6b27f",
+    "url": "/facebookclone/static/js/2.df10d3cb.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/facebookclone/static/js/2.df10d3cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d05bf40d0534acd2ed9",
+    "url": "/facebookclone/static/js/main.dc45c541.chunk.js"
+  },
+  {
+    "revision": "912e5456fa73f930beab",
+    "url": "/facebookclone/static/js/runtime-main.bc1d8367.js"
+  }
+]);
